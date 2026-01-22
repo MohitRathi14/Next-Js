bcde74efc42d1453614848c96a06957267a5c502
MohitRathi14 <aaditya375singh2@gmail.com>
2026-01-22 11:22:28 +0530

    feat: update GenralProduct component to use client-side rendering and add navigation button

--- myfirstapp/src/app/products/[slug]/page.js
+++ myfirstapp/src/app/products/[slug]/page.js
@@ -1,6 +1,15 @@
+'use client';
+import React from "react";
 import { products } from "../data";
-export default async function GenralProduct({ params }) {
-  let { slug } = await params;
+import { useRouter } from "next/navigation"; 
+
+// export default async function GenralProduct({ params }) { //for server component
+
+
+export default function GenralProduct({ params }) {
+  // let { slug } = await params; //for server component
+  let {slug} = React.use(params);
+  const router = useRouter();
   let filteredProduct = products.filter(
     (product) =>
       product.category.toLocaleLowerCase() === slug.toLocaleLowerCase()
@@ -18,6 +27,7 @@ export default async function GenralProduct({ params }) {
                 </div>
             ))
         }
+        <button className='text-center bg-blue-300 rounded-lg text-amber-50 p-1.5 hover:bg-blue-400 hover:p-2 ' onClick={()=>router.push('/products')}>Go to Product Page</button>
     </>
   );
 }
